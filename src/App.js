414bd24daaa8739d98b4868f6a708f53da2ab79a
SHIMIL T <shimiltharayil007@gmail.com>
2022-09-06 13:03:48 +0530

    redux added

--- src/App.js
+++ src/App.js
@@ -3,15 +3,24 @@ import Header from './app/Components/Header';
 import './App.css';
 import Sidebar from '../src/app/Components/Sidebar'
 import Feed from '../src/app/Components/Feed';
+import { useSelector } from 'react-redux';
+import { selectUser } from './features/counter/userSlice';
+import Login from '../src/app/Components/Login';
 
 function App() {
+  const user = useSelector(selectUser)
   return (
     <div className="app">
            <Header />
-           <div className="app__body">
+           {!user ? (
+            <Login />
+           ): (
+
+             <div className="app__body">
             <Sidebar />
             <Feed />
            </div>
+             )}
     </div>
   );
 }
---
+++ src/app/Components/Login.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import "./CSS/Login.css"
+
+function Login() {
+  return (
+    <div className='login'></div>
+  )
+}
+
+export default Login;
